274c33fe901a720fa2ec438d28b8a3fbaa71fae1
radek <rajkonkret660@gmail.com>
2026-03-10 10:54:36 +0100

    [1 9] add wczytanie_csv.py

--- day2/wczytanie_csv.py
+++ day2/wczytanie_csv.py
@@ -1,6 +1,22 @@
 import csv
 
+fileds = []
+rows = []
+
 with open("bmw_global_sales_2018_2025.csv") as f:
+    dialect = csv.Sniffer().sniff(f.read(1024))
+    print(dialect.delimiter)  # ,
+    print(dialect.quotechar)  # "
+
+    f.seek(0) # odczyt na początek
     csvreader = csv.reader(f)
 
     print(csvreader)
+
+    fileds = next(csvreader)
+
+    for row in csvreader:
+        rows.append(row)
+
+print('Fields:', fileds)
+print('Rows:', rows[0])
