5cb95c45fab595c261bb312ae860a441c532412a
radek <rajkonkret660@gmail.com>
2026-03-11 15:00:40 +0100

    [3 17] update funjkcja_call

--- day3/funkcja_call/main.py
+++ day3/funkcja_call/main.py
@@ -6,6 +6,8 @@ Trzy przypadki użycia funkcji __call__
 """
 
 from counter_zad1 import CallCounter
+from validator import RangeValidator
+from cache import CacheFunction
 
 print(70 * "-")
 counter = CallCounter()
@@ -21,3 +23,44 @@ counter()
 # funkcję call wywołano 3 razy
 # funkcję call wywołano 4 razy
 # funkcję call wywołano 5 razy
+
+print(70 * "-")
+valid = RangeValidator(2, 20)
+valid(3)
+valid(17)
+valid(8)
+valid(44)
+
+print(70 * "-")
+
+
+def efunction(x, y):
+    return x ** y + y ** x
+
+
+ce_func = CacheFunction(efunction)
+print(ce_func(2, 3))
+print(ce_func(2, 3))
+print(ce_func(2, 3))
+print(ce_func(2, 3))
+print(ce_func(2, 3))
+print(ce_func(12, 1))
+print(ce_func(12, 1))
+print(ce_func(2, 3))
+# ----------------------------------------------------------------------
+# wynik obliczeń dla argumentów: (2, 3)
+# 17
+# wynik w cache dla argumentów: (2, 3)
+# 17
+# wynik w cache dla argumentów: (2, 3)
+# 17
+# wynik w cache dla argumentów: (2, 3)
+# 17
+# wynik w cache dla argumentów: (2, 3)
+# 17
+# wynik obliczeń dla argumentów: (12, 1)
+# 13
+# wynik w cache dla argumentów: (12, 1)
+# 13
+# wynik w cache dla argumentów: (2, 3)
+# 17
