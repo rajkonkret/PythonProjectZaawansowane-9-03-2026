d82620994a2d562ecb83d94d69be288b795652ea
radek <rajkonkret660@gmail.com>
2026-03-11 14:51:58 +0100

    [3 16] update funjkcja_call

---
+++ day3/funkcja_call/cache.py
@@ -0,0 +1,15 @@
+class CacheFunction:
+
+    def __init__(self, func):
+        self.func = func
+        self.cache = {}
+
+    def __call__(self, *args):
+        if args in self.cache:
+            print(f'wynik w cache dla argumentów: {args}')
+            return self.cache[args]
+
+        print(f"wynik obliczeń dla argumentów: {args}")
+        result = self.func(*args)
+        self.cache[args] = result
+        return result
