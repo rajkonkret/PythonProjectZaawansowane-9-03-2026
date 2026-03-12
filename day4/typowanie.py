8f0fb82c97ea56ab2c6b597351c7a7faf942a9a0
radek <rajkonkret660@gmail.com>
2026-03-12 12:13:47 +0100

    [3 25] update propery_osoba

--- day4/typowanie.py
+++ day4/typowanie.py
@@ -49,3 +49,81 @@ def validate_coordinate(coord: Coordinate) -> bool:
 
 example_path = [(0.0, 1.0), (2.5, 3.5), (4.0, -1.2), ("True", True)]
 print(validate_coordinate(example_path[-1]))
+
+# Protocol
+from typing import Protocol
+
+
+class Runner(Protocol):
+    def run(self) -> str:
+        ...
+
+    def finish_time(self) -> float:
+        ...
+
+
+class Athlete:
+    def run(self) -> str:
+        return "Athlete is running"
+
+    def finish_time(self):
+        return 1.15
+
+
+class Robot:
+    def run(self) -> str:
+        return "robot is running"
+
+    def finish_time(self) -> float:
+        return 1.12
+
+
+def start_race(participiant: Runner) -> None:
+    print(participiant.run())
+    print(participiant.finish_time())
+
+
+athlete = Athlete()
+robot = Robot()
+start_race(athlete)
+start_race(robot)
+# Athlete is running
+# 1.15
+# robot is running
+# 1.12
+
+# generic
+from typing import TypeVar, Generic, List
+
+T = TypeVar('T')
+
+
+class Stack(Generic[T]):
+    def __init__(self) -> None:
+        self._container: List[T] = []
+
+    def push(self, item: T) -> None:
+        self._container.append(item)
+
+    def pop(self) -> T:
+        return self._container.pop()
+
+
+stack = Stack[int]()
+stack.push(10)
+stack.push(12)
+stack.push(8.8)
+stack.push("abcz")
+print(stack.pop())
+
+
+# nowsze podejście
+class Stack[V]:
+    def __init__(self) -> None:
+        self._container: list[V] = []
+
+    def push(self, item: V) -> None:
+        self._container.append(item)
+
+    def pop(self) -> V:
+        return self._container.pop()
