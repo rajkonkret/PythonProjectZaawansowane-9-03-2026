3ec4a1ccdd141a383af292f5bd7687d77331f9ca
radek <rajkonkret660@gmail.com>
2026-03-12 15:48:25 +0100

    [4 19] update futures_zad1.py

---
+++ day4/futures_zad1.py
@@ -0,0 +1,28 @@
+import concurrent.futures
+import time
+
+
+def task(n):
+    print(f"start zadania {n}")
+    time.sleep(2)
+    print(f"Koniec zdarzenia {n}")
+
+    return n * 2
+
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+    futures = [executor.submit(task, i) for i in range(5)]
+    results = [future.result() for future in concurrent.futures.as_completed(futures)]
+
+    print(f"wynik: {results}")
+# start zadania 0
+# start zadania 1
+# start zadania 2
+# start zadania 3
+# Koniec zdarzenia 0
+# start zadania 4
+# Koniec zdarzenia 2
+# Koniec zdarzenia 3
+# Koniec zdarzenia 1
+# Koniec zdarzenia 4
+# wynik: [0, 4, 6, 2, 8]
