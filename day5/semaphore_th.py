47102a459acab7a5eb9863f94a230b6aa21653e3
radek <rajkonkret660@gmail.com>
2026-03-13 11:53:22 +0100

    [5 12] update semaphore_th.py

---
+++ day5/semaphore_th.py
@@ -0,0 +1,30 @@
+import threading
+import time
+import random
+
+semaphore = threading.Semaphore(3)
+
+
+def access_resource(thread_id):
+    print(f"Wątek {thread_id} czeka na dostep do zasobu...")
+    semaphore.acquire()
+    try:
+        print(f"Wątek {thread_id} uzyskał dostep do zasobu...")
+        time_to_sleep = random.uniform(1, 3)
+        time.sleep(time_to_sleep)
+        print(f"Wątek {thread_id} zwalnia zasób po {time_to_sleep} sekundach")
+    finally:
+        semaphore.release()
+
+
+threads = []
+
+for i in range(10):
+    t = threading.Thread(target=access_resource, args=(i,))
+    threads.append(t)
+    t.start()
+
+for t in threads:
+    t.join()
+
+print("Wszystkie wątki zostały zakończone")
