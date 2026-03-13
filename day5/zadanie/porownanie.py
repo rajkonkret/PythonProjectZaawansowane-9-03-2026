578d00d60e261409b15ff1e3c29687de425807bf
radek <rajkonkret660@gmail.com>
2026-03-13 09:33:54 +0100

    [5 3] update porownanie.py

--- day5/zadanie/porownanie.py
+++ day5/zadanie/porownanie.py
@@ -1,21 +1,19 @@
 import time
 import concurrent.futures
+import multiprocessing
+import os
 
 from funkcja_prime import find_the_sum_prime_nums
 
 numbers = [(1, 10_000), (3, 50_000), (5_000, 100_000), (4, 9000), (8_000, 15_000), (95_000, 133_000), (200, 67_340)]
 
 
-def synchronicznie():
+def multi():
     starttime = time.time()
-    for pair in numbers:
-        st = time.time()
-        prime_suma = find_the_sum_prime_nums(*pair)
-        et = time.time()
-        print(f"Wynik = {prime_suma}, czas wykonania: {et - st} s")
-    endtime = time.time()
-
-    print(f"Całkowity czas wykonania wszystkich sum (synchronicznie): {endtime - starttime}")
+    with multiprocessing.Pool(processes=4) as pool:
+        result = pool.map(run_heavt_function, numbers)
+    end_time = time.time()
+    print(f"Całkowity czas wykonania wszystkich sum (multi process): {end_time - starttime}")
 
 
 def run_heavt_function(params):
@@ -31,18 +29,11 @@ def asymchronicznie():
 
 
 def main():
-    synchronicznie()
+    multi()
     asymchronicznie()
 
 
 if __name__ == '__main__':
     main()
-# Wynik = None, czas wykonania: 0.1360936164855957 s
-# Wynik = None, czas wykonania: 2.896277666091919 s
-# Wynik = None, czas wykonania: 11.040292978286743 s
-# Wynik = None, czas wykonania: 0.1107187271118164 s
-# Wynik = None, czas wykonania: 0.20446014404296875 s
-# Wynik = None, czas wykonania: 9.070082187652588 s
-# Wynik = None, czas wykonania: 5.185152292251587 s
-# Całkowity czas wykonania wszystkich sum (synchronicznie): 28.64328932762146
-# Całkowity czas wykonania wszystkich sum (asynchronicznie): 13.506874561309814
+# Całkowity czas wykonania wszystkich sum (multi process): 12.02274775505066
+# Całkowity czas wykonania wszystkich sum (asynchronicznie): 12.259918212890625
